3bb83fd43278333f8b6f80a661757a1b0717a5f9
Brian Wastle <brian.wastle1@gmail.com>
2024-12-01 21:46:22 -0500

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    { provide: LocationStrategy, useClass: PathLocationStrategy }  // Apply the PathLocationStrategy
+  ]
+})
   .catch((err) => console.error(err));
